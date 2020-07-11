bd36b195f54201261fe3cc104c4500f937500ca9
Xiaoyu Tai <taixiaoyu@gmail.com>
2020-07-10 22:24:15 -0700

    Initial Commit

---
+++ SetGame/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SetGame
+//
+//  Created by Sean Tai on 7/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Sean Tai on 7/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
